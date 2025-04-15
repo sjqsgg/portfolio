af6d89c38e78e2865db4977515be68b5c4e21efa
jiaqi <jiaqii7@outlook.com>
2025-04-15 10:08:16 +0200

    Update: small fix in logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
       className="flex flex-col items-center w-full fixed top-0 z-50 bg-white"
         >
                     <div className="logo-wrapper p-4">
-                        <img src="../IMG_5410.JPG" alt="logo" style={{ height: '90px' }} />
+                        <img src="/photo/IMG_5410.JPG" alt="logo" style={{ height: '90px' }} />
                     </div>
 
                     <ul className="nav grid grid-flow-col grid-rows-1 justify-content-center pt-3 pb-0 gap-8">
